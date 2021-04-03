e77292ccf04f4c823a342183011c6c41648bfa62
Serafim Lentas <sir.lentas@gmail.com>
2021-04-03 15:07:01 +0300

    version 1.0

---
+++ translator.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    setlocale(LC_CTYPE, "");
+    wchar_t br = 0x2800;
+    char line[1200];
+    int value;
+    char *c1;
+    fgets(line, 120, stdin);
+    printf("6dots:\n %s", line);
+    printf("Braille:\n");
+    c1 = strtok(line, "-\n\0");
+    while (c1)
+    {
+        if (strcmp(c1, "line") == 0)
+        {
+            printf("\n");
+        }
+        else if (strcmp(c1, "0") == 0)
+        {
+            printf("%lc", 0x2800);
+        }
+        else if (atoi(c1) >= 1 && atoi(c1) <= 123456)
+        {
+            int number=atoi(c1);
+            value = 0;
+            while(number)
+            {
+                int digit=number%10;
+                value+=pow(2,digit-1);
+                number /= 10;
+            }
+            printf("%lc", br+value);
+        }
+        c1 = strtok(NULL, "-\n\0");
+    }
+    printf("\n");
+}
